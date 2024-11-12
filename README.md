6b9816e19398694cacd48cdf3e1ca083bc61cdd9
jhskrt <jhskrt@users.noreply.github.com>
2024-11-12 07:36:19 +0000

    deploy: ff3d70e1470471358abbca1ec2f5dff1059733c7

---
+++ README.md
@@ -0,0 +1,93 @@
+# dark-and-darker-damage-calculater
+
+## How to download 如何下載
+點擊右側 Releases 下載最新版本的 dndDmgCalculater
+
+下載完成後打開程式即可使用
+
+![image](https://github.com/user-attachments/assets/adbdc135-bcac-4e90-9cde-0ecd506b143c)
+
+
+## How to use 如何使用
+在對應的輸入欄位輸入數值並按下 "計算" 按鈕即可獲得結果
+
+> [!warning]
+> 若無法輸入小數點，請切換輸入法再試試看
+
+## Input explanation 輸入欄位解釋
+
+> [!IMPORTANT]
+> 物理強度加成、護甲穿透以及傷害減免欄位請輸入小數
+
+`武器傷害(Weapon Damage)`： 武器的基礎傷害 + 天賦給的武器傷害(詩人西洋劍精通、戰歌、野蠻人斧頭專精等等)
+
+`受擊部位(Hit Location)`： 打頭造成 1.5x 、身體 1.0x 、手臂0.8x 、腿0.6x 、手/腳 0.5x 傷害
+
+`物理/魔法強度加成(Physical Power Bonus)`： 可以直接由角色細則查看，此數值受力量/意志、物理/魔法強度以及物理/魔法傷害加成影響，其中1力量/意志 = 1物理/魔法強度
+
+`物理強度`：物理強度加成請參考 [物理強度 wiki](<https://darkanddarker.wiki.spellsandguns.com/Stats#Physical_Power>)
+
+![image](https://github.com/user-attachments/assets/7adae393-d79c-40ad-9918-6da0c4c36cb2)
+
+`魔法強度`：魔法強度加成請參考 [魔法強度 wiki](<https://darkanddarker.wiki.spellsandguns.com/Stats#Magical_Power>)
+
+![image](https://github.com/user-attachments/assets/fa1396af-2520-4f95-bd8f-cdd381bf7f89)
+
+`額外/真實物理(魔法)傷害(Additional/True Damage)`： 裝備詞條提供的額外/真實傷害
+
+`護甲/魔法穿透(Armor/Magic Penetration)`： 裝備詞條提供的護甲/魔法穿透
+
+`額外武器傷害(Addition Weapon Damage)`： 武器詞條提供的額外武器傷害
+
+`物理傷害減免/魔法抗性(Physical Damage Reduction/Magic Resistance)`： 由護甲值和裝備詞條決定的物理傷害減免，可以直接由角色細則查看，護甲值：傷害減免請參考 [護甲值 wiki](<https://darkanddarker.wiki.spellsandguns.com/Stats#Armor_Rating>)
+
+> [!note]
+> 假人(Dummy)的物抗是-12% 魔抗-20%，其他怪物雙防請參考 [怪物 wiki](https://darkanddarker.wiki.spellsandguns.com/Monsters#Dummy)
+
+## Damage Formula 傷害公式
+```
+(
+  (
+    (
+        + (Base Damage + Buff Weapon Damage) * Combo Multiplier * Impact Zone Multiplier
+        + Gear Weapon Damage|Magical Damage
+        + Divine Strike Damage
+    )
+    * (1 + Power Bonus)
+    + Additional Damage
+  )
+  * (1 + Hit Location Bonus)
+  * (1 + Race Damage Bonus)
+  * (1 - Race Damage Reduction)
+  * (1 - Damage Reduction * (1 + Damage Reduction Mod) * (1 - Penetration))
+  * (1 - Projectile Reduction)
+)
+* Projectile Falloff
++ True Damage
+```
+> [!note]
+> 此公式為 wiki 提供，與遊戲內翻譯不同，各變數解釋請參考 wiki
+
+[傷害公式 wiki](<https://darkanddarker.wiki.spellsandguns.com/Damage>)
+
+## Source 資料來源
+
+以上資料皆來自 https://darkanddarker.wiki.spellsandguns.com/Dark_and_Darker_Wiki
+
+## QnA
+
+Q1 : 我有發現問題或建議該如何反應
+
+A1 : 歡迎發 Issues 討論
+
+
+
+Q2 : 算出來的傷害怎麼會有小數點?
+
+A2 : 遊戲內傷害跟生命都有小數點，只是顯示會取整數而已，生命值無條件進位，假人傷害無條件捨去，請參考 [四捨五入 wiki](<https://darkanddarker.wiki.spellsandguns.com/Damage#Rounding>)
+
+
+
+Q3 : 參考了 Q2 但算出來還是錯的阿
+
+A3 : 那應該是我寫錯了，本程式並沒有經過嚴謹的驗算和證實，如有誤歡迎提 Issues 討論
---
+++ dndDmgCalculater.py
@@ -0,0 +1,102 @@
+import tkinter as tk
+
+last_result = [0]
+def calculate_damage():
+    try:
+        # 傷害
+        base_damage = float(entry_base_damage.get()) # 武器基礎傷害
+        power_bonus = float(entry_power_bonus.get()) # 物理強度加成
+        additional_damage = float(entry_additional_damage.get())   # 額外物理(魔法)傷害
+        true_damage = float(entry_true_damage.get()) # 真實外物理(魔法)傷害
+        penetration = float(entry_penetration.get()) # 護甲穿透
+        gear_weapon_damage = float(entry_gear_weapon_damage.get()) # 額外武器傷害
+
+        hit_location_bonus = float(entry_hit_location_bonus.get()) # 受擊部位 頭1.5 身1.0 臂.8 腿.6 手/腳.5
+        buff_weapon_damage = 0.0 # 天賦影響的加成(詩人戰歌等)
+
+        # 減傷
+        damage_reduction = float(entry_damage_reduction.get())
+        damage_reduction_mod = 1.0 # i have no idea what is this
+
+        projectile_reduction = 0.0 # 飛行物傷害減免
+        projectile_falloff = 1.0   # 飛行物距離衰減
+        
+        # 雜項
+        combo_multiplier = 1.0 # 連擊獎勵
+        impact_zone_multiplier = 1.0 # 武器打擊區域
+        divine_strike_damage = 0.0 # 牧師技能神擊
+
+        damage = (
+            (
+                (
+                    (base_damage + buff_weapon_damage) * combo_multiplier * impact_zone_multiplier
+                    + gear_weapon_damage + divine_strike_damage
+                ) * (1 + power_bonus) + additional_damage
+            ) * hit_location_bonus
+              * (1 - damage_reduction * (1 + damage_reduction_mod) * (1 - penetration))
+              * (1 - projectile_reduction)
+        ) * projectile_falloff + true_damage
+
+        last_result.append(damage)
+        label_result.config(text=f"計算結果(前次結果): {damage}({last_result[-2]})")
+        last_result.pop(0)
+    
+    except ValueError:
+        label_result.config(text="輸入格式錯誤!")
+
+root = tk.Tk()
+root.title("Damage Calculator")
+
+msg = """
+目前因為魔法強度計算與物理強度計算方法不同
+因此魔法強度計算會有誤差 但結果還是可以參考
+"""
+tk.Label(root, text = msg).grid(row=0, column=0)
+
+tk.Label(root, text="武器傷害:").grid(row=1, column=0)
+entry_base_damage = tk.Entry(root)
+entry_base_damage.insert(0, 1)
+entry_base_damage.grid(row=1, column=1)
+
+tk.Label(root, text="受擊部位:").grid(row=2, column=0)
+entry_hit_location_bonus = tk.Entry(root)
+entry_hit_location_bonus.insert(0, 1.5)
+entry_hit_location_bonus.grid(row=2, column=1)
+
+tk.Label(root, text="物理(魔法)強度加成:").grid(row=3, column=0)
+entry_power_bonus = tk.Entry(root)
+entry_power_bonus.insert(0, 0)
+entry_power_bonus.grid(row=3, column=1)
+
+tk.Label(root, text="額外物理(魔法)傷害:").grid(row=4, column=0)
+entry_additional_damage = tk.Entry(root)
+entry_additional_damage.insert(0, 0)
+entry_additional_damage.grid(row=4, column=1)
+
+tk.Label(root, text="真實物理(魔法)傷害:").grid(row=5, column=0)
+entry_true_damage = tk.Entry(root)
+entry_true_damage.insert(0, 0)
+entry_true_damage.grid(row=5, column=1)
+
+tk.Label(root, text="護甲(魔法)穿透:").grid(row=6, column=0)
+entry_penetration = tk.Entry(root)
+entry_penetration.insert(0, 0)
+entry_penetration.grid(row=6, column=1)
+
+tk.Label(root, text="額外武器傷害:").grid(row=7, column=0)
+entry_gear_weapon_damage = tk.Entry(root)
+entry_gear_weapon_damage.insert(0, 0)
+entry_gear_weapon_damage.grid(row=7, column=1)
+
+tk.Label(root, text="物理傷害減免/魔法抗性:").grid(row=8, column=0)
+entry_damage_reduction = tk.Entry(root)
+entry_damage_reduction.insert(0, 0)
+entry_damage_reduction.grid(row=8, column=1)
+
+btn_calculate = tk.Button(root, text="計算", command=calculate_damage)
+btn_calculate.grid(row=9, column=0, columnspan=2)
+
+label_result = tk.Label(root, text="計算結果(前次結果): ")
+label_result.grid(row=10, column=0, columnspan=2)
+
+root.mainloop()
